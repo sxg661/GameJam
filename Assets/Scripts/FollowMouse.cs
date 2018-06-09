9e803196bbc6988c89fea256462d5bbbaea2d0b9
sxg661 <sxg661@student.bham.ac.uk>
2018-06-09 16:23:20 +0100

    added a hammer

---
+++ Assets/Scripts/FollowMouse.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowMouse : MonoBehaviour {
+
+    private Vector3 mousePosition;
+    public float moveSpeed = 10f;
+    Animator anim;
+
+    
+    // Use this for initialization
+    void Start () {
+        anim = GetComponent<Animator>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        mousePosition = Input.mousePosition;
+        mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
+        transform.position = Vector2.Lerp(transform.position, mousePosition, moveSpeed);
+
+        if (Input.GetMouseButtonDown(0))
+        {
+
+            anim.SetTrigger("HammerHit");
+            
+        }
+        
+    }
+}
