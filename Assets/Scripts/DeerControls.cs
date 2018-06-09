0822e8af16bfa6fc05d55cc2dc1fa1ca355ef752
sxg661 <sxg661@student.bham.ac.uk>
2018-06-09 14:00:22 +0100

    added a load of assets and made my own deer prefab

---
+++ Assets/Scripts/DeerControls.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeerControls : MonoBehaviour {
+
+    public float maxSpeed = 10f;
+    public bool directionRight = false ;
+    public Rigidbody2D deer;
+    public Animator anim;
+
+	// Use this for initialization
+	void Start () {
+		deer = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+    }
+	
+	// In sync with animator
+	void FixedUpdate () {
+        float move = Input.GetAxis("Horizontal");
+
+        //mantains its y velocity, but the x velocity is any number (pos or neg) up to the max speed
+        deer.velocity = new Vector2(move * maxSpeed, deer.velocity.y);
+
+        anim.SetFloat("Speed", Mathf.Abs(move));
+        
+        //if the direction of velocity has changed then we need to do a flip
+        if(move < 0 && directionRight){
+            Debug.Log("flip left");
+            Flip();
+        }
+        else if(move > 0 && !directionRight){
+            Debug.Log("flip right");
+            Flip();
+        }
+
+	}
+
+    void Flip ()
+    {
+        directionRight = !directionRight;
+        anim.SetBool("Right", directionRight);
+    }
+}
